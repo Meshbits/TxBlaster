ead7e25c0300f8b026256ab32209dcca6d2838c6
satindergrewal <grewal@lavabit.com>
2018-05-16 02:44:04 +1200

    missing ac_conf reader file

---
+++ private/js/ac_conf.js
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2015 Satinderjit Singh
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+var child_process = require('child_process'),
+    fs = require('fs-extra'),
+    path = require('path'),
+    os = require('os');
+
+
+var ac_conf = module.exports = {
+  exec: child_process.exec,
+  status: status
+};
+
+/**
+ * Parses the status for a single network interface.
+ *
+ * @private
+ * @static
+ * @category ac_conf
+ * @returns {object} The parsed network interface status.
+ *
+ */
+function parse_status_block(block) {
+  var match;
+
+  var parsed = {
+    rpcuser: block.match(/rpcuser=([^\s]+)/)[1]
+  };
+
+  if ((match = block.match(/rpcpassword=\s*(.*)/))) {
+    parsed.rpcpass = match[1];
+  }
+
+  if ((match = block.match(/rpcport=\s*(.*)/))) {
+    parsed.rpcport = match[1];
+  }
+
+  if ((match = block.match(/server=\s*(.*)/))) {
+    parsed.server = match[1];
+  }
+
+  return parsed;
+}
+
+/**
+ * Parses the status for all network interfaces.
+ *
+ * @private
+ * @static
+ * @category ac_conf
+ * @param {function} callback The callback function.
+ *
+ */
+function parse_status(callback) {
+  return function(error, stdout, stderr) {
+    if (error) callback(error);
+    else callback(error,
+      stdout.trim().split('\n\n').map(parse_status_block));
+      //stdout.trim().split(/\s\*-usb:/g).map(parse_status_block));
+      //console.log(stdout.trim().split(/\s\*-usb:/g))
+  };
+}
+
+/**
+ * The **ac_conf status** command is used to query the status of all
+ * configured interfaces.
+ *
+ * @static
+ * @category ac_conf
+ * @param {function} callback The callback function.
+ * @example
+ *
+ * var ac_conf = require('./ac_conf');
+ *
+ * ac_conf.status(function(err, status) {
+ *   console.log(status);
+ * });
+ *
+ * // =>
+ * [
+ *  {
+ *    "rpcuser": "pi",
+ *    "rpcpass": "c892f76fc45365d50cb744ec1d5cdde659c98e0b",
+ *    "rpcport": "8336",
+ *    "server": "1"
+ *  }
+ * ]
+ *
+ */
+function status(coin_name, interface, callback) {
+  //console.log(coin_name);
+  this.exec(`cat ${process.env.HOME}/.komodo/${coin_name}/${coin_name}.conf`, parse_status(interface));
+}
